639df17675e16eb86d8605cf6d43d2df33a5fed9
Andy Zhao <andyzhao.zhao@gmail.com>
2017-01-02 17:07:34 -0500

    commiting halfway through testing chapter

---
+++ spec/rails_helper.rb
@@ -0,0 +1,65 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+# Prevent database truncation if the environment is production
+abort("The Rails environment is running in production mode!") if Rails.env.production?
+require 'spec_helper'
+# Add additional requires below this line. Rails is not loaded until this point!
+require 'rspec/rails'
+require 'capybara/poltergeist'
+
+Capybara.javascript_driver = :poltergeist
+Capybara.default_driver = :poltergeist
+
+Capybara.register_driver :poltergeist do |app|
+  Capybara::Poltergeist::Driver.new(app, {:js_errors => false})
+end
+
+
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.use_transactional_fixtures = false
+  config.infer_spec_type_from_file_location!
+
+  config.before(:suite) do
+    $pid = spawn("./node_modules/.bin/webpack-dev-server " +
+                 "--config config/webpack.config.js --quiet")
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.after(:suite) do
+    puts "Killing webpack-dev-server"
+    Process.kill("HUP",$pid)
+    begin
+      Timeout.timeout(2) do
+        Process.wait($pid,0)
+      end
+    rescue => Timeout::Error
+      Process.kill(9,$pid)
+    end
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, :type => :feature) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do
+    DatabaseCleaner.start
+  end
+
+  config.after(:each) do
+    DatabaseCleaner.clean
+  end
+
+  # Filter lines from Rails gems in backtraces.
+  config.filter_rails_from_backtrace!
+  # arbitrary gems may also be filtered via:
+  # config.filter_gems_from_backtrace("gem name")
+end
--- webpack/application.js
+++ webpack/application.js
@@ -14,6 +14,42 @@ var ng                  = {
   http:                   require("@angular/http")
 };
 
+var AngularTestComponent = ng.core.Component({
+  selector: "shine-angular-test",
+  template: '\
+  <h2 *ngIf="name">Hello {{name}}!</h2> \
+  <form> \
+    <div class="form-group"> \
+      <label for="name">Name</label> \
+      <input type="text" id="name" class="form-control" \
+             name="name" bindon-ngModel="name"> \
+    </div> \
+  </form> \
+  '
+}).Class({
+  constructor: function() {
+    this.name = null;
+  }
+});
+
+var AngularTestAppModule = ng.core.NgModule({
+  imports: [ ng.platformBrowser.BrowserModule, ng.forms.FormsModule ],
+  declarations: [ AngularTestComponent ],
+  bootstrap: [ AngularTestComponent ]
+})
+.Class({
+  constructor: function() {}
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var shouldBootstrap = document.getElementById("angular-test");
+  if (shouldBootstrap) {
+    ng.platformBrowserDynamic.
+      platformBrowserDynamic().
+      bootstrapModule(AngularTestAppModule);
+  }
+});
+
 var CustomerSearchComponent = ng.core.Component({
   selector: "shine-customer-search",
   template: '\
@@ -30,7 +66,7 @@ var CustomerSearchComponent = ng.core.Component({
                class="form-control input-lg" > \
     </form> \
   </section> \
-  <section class="search-results"> \
+  <section class="search-results" *ngIf="customers"> \
     <header> \
       <h1 class="h3">Results</h1> \
     </header> \
