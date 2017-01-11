3aaad8f28c381974398c1cfed3b19998b17707a3
Andy Zhao <andyzhao.zhao@gmail.com>
2017-01-11 06:44:12 -0500

    added rspec and jasmine testing specs

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -9,7 +9,7 @@ require 'rspec/rails'
 require 'capybara/poltergeist'
 
 Capybara.javascript_driver = :poltergeist
-Capybara.default_driver = :poltergeist
+Capybara.default_driver    = :poltergeist
 
 # Capybara.register_driver :poltergeist do |app|
 #   Capybara::Poltergeist::Driver.new(app, {:js_errors => false})
